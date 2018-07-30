7c5fc430a12e0c322458358a5e7a6a3d17e61dc1
briecodes <hello@brie.work>
2018-07-30 16:19:53 -0400

    continued styling of instructions.

--- src/components/InstructionsScreen.js
+++ src/components/InstructionsScreen.js
@@ -7,7 +7,17 @@ class InstructionsScreen extends Component {
       <React.Fragment>
         <div id='instructions-logo'></div>
         <div id='instructions-title'></div>
+        <div id='instructions-copy'>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam.
+        </div>
+        <div id='pig-detail'></div>
+        <div id='cock-detail'></div>
+        <div id='snake-detail'></div>
         <span className='x text-button' onClick={this.props.toggleInstructions}>X</span>
+        <div id='dot-holder'>
+          <div className='dot'></div>
+          <div className='dot'></div>
+        </div>
         <div id='clouds'></div>
         <div id='mandala'></div>
       </React.Fragment>
